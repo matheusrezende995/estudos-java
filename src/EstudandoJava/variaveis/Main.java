7df5826480cf04648ea2aac39daf51a1af90ce0e
matheusrezende995 <matheus995rezende@gmail.com>
2026-09-04 23:37:10 -0300

    .

--- src/EstudandoJava/variaveis/Main.java
+++
@@ -1,7 +0,0 @@
-package EstudandoJava.variaveis;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Deu certo! O Java tá funcionando!");
-    }
-}
---
+++ src/estudandojava/variaveis/Main.java
@@ -0,0 +1,20 @@
+package estudandojava.variaveis;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
