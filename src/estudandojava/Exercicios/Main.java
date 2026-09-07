c313f412e31dbcec83b135a85421a34004907430
matheusrezende995 <matheus995rezende@gmail.com>
2026-09-07 17:04:12 -0300

    adicionei mais exercícios + calculadora simples

--- src/estudandojava/Exercicios/Main.java
+++ src/estudandojava/Exercicios/Main.java
@@ -298,15 +298,34 @@ public class Main {
         }
         */
 
+        /*
+----------------------------------------------------Exercício 14:-------------------------------------------------------
+
+        Enunciado: Desenvolva um programa que leia cinco números inteiros do usuário, um por
+        vez, e acumule a soma deles usando o operador de atribuição +=. Exiba o total acumulado
+        ao final.
+        Dica: Utilize o Scanner para pedir os dados, e você pode utilizar um loop for para repetir a
+        solicitação de dados cinco vezes
 
 
 
 
+        int somaAcumulada = 0;
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Digite o: " + i + "° número inteiro: ");
+            int valorDigitado = scanner.nextInt();
+            somaAcumulada += valorDigitado;
+        }
 
-
+        System.out.println("Soma acumulada: " + somaAcumulada);
+        */
 
 
 
         scanner.close();
     }
-}
+
+
+
+    }
+
---
+++ src/miniprojetosjava/CalculadoraSimples.java
@@ -0,0 +1,78 @@
+package miniprojetosjava;
+import java.util.Scanner;
+
+public class CalculadoraSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /**
+         * Objetivo: Criar uma calculadora que faz soma, multiplicação, divisão e subtração
+         * 1 - Pedir dois números para o usuário (double)
+         * 2 - Apresentar uma tabela/texto para escolher a operação
+         * 3 - Resgatar a operação que o usuário selecionou
+         * 4 - Realizar o calculo
+         * 5 - Exibir o resultado
+         * 6 - Se a operação escolhida for inválida, exibir mensagem de erro
+         */
+
+        System.out.println("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Escolha a opção que deseja realizar com os números digitados: ");
+        System.out.println("1- Somar");
+        System.out.println("2- Subtrair");
+        System.out.println("3- Dividir");
+        System.out.println("4- multiplicar");
+        int opcao = scanner.nextInt();
+
+        switch (opcao) {
+            case 1:
+                System.out.println("A soma é: " + soma(num1, num2));
+                break;
+            case 2:
+                System.out.println("A subtração é: " + subtracao(num1, num2));
+                break;
+            case 3:
+                System.out.println("A divisão é: " + divisao(num1, num2));
+                break;
+            case 4:
+                System.out.println("A multiplicação é: " + multiplicacao(num1, num2));
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+
+
+
+
+
+
+
+
+        scanner.close();
+    }
+
+    public static double soma (double num1 , double num2) {
+        double resultado = num1 + num2;
+        return resultado;
+    }
+
+    public static double multiplicacao (double num1 , double num2) {
+        double resultado = num1 * num2;
+        return resultado;
+    }
+
+    public static double divisao (double num1 , double num2) {
+        double resultado = num1 / num2;
+        return resultado;
+    }
+
+    public static double subtracao (double num1 , double num2) {
+        double resultado = num1 - num2;
+        return resultado;
+    }
+
+
+}
